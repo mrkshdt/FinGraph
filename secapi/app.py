208e07908472c9718f1a1325cd121caf74dc9c4a
markus.heidt <M_Heidt@web.de>
2023-07-10 18:01:24 +0200

    bug fix

--- secapi/app.py
+++ secapi/app.py
@@ -12,6 +12,8 @@ from nltk.stem import WordNetLemmatizer
 from gensim.models import Word2Vec
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
+from scipy.sparse.csgraph import minimum_spanning_tree
+import networkx as nx
 
 
 
@@ -69,11 +71,13 @@ def get_business_description(df, api_key, extractorApi):
         url_10k = row['linkToTxt']
         try:
             description_text = extractorApi.get_section(url_10k, "1", "text")
-            result_df = result_df.append({'ticker': ticker, 'description': description_text}, ignore_index=True)
+            new_row = pd.DataFrame({'ticker': [ticker], 'description': [description_text]})
+            result_df = pd.concat([result_df, new_row], ignore_index=True)
         except Exception as e:
             print(f"Error while fetching description for {ticker}: {str(e)}")
 
     return result_df
+
 
 def preprocess_text(text):
     # Convert the text to lowercase
@@ -123,6 +127,22 @@ def get_avg_word_vector(words, model, num_features):
 
     return feature_vector
 
+def create_minimum_spanning_tree(similarity_matrix, result):
+    distance_matrix = 1 - similarity_matrix
+
+    # Compute the minimal spanning tree
+    sparse_tree = minimum_spanning_tree(distance_matrix)
+
+    # Convert the sparse tree to a dense tree
+    dense_tree = sparse_tree.toarray()
+
+    # Create a mapping from node indices to tickers
+    index_to_ticker = {i: ticker for i, ticker in enumerate(result['ticker'])}
+
+    # Create a graph from the dense tree
+    G = nx.relabel_nodes(nx.DiGraph(dense_tree), index_to_ticker)
+
+    return G
 
 def main():
     # Read the configuration file
@@ -139,6 +159,13 @@ def main():
     tickers = ['A', 'AAL', 'AAP', 'AAPL', 'ZION', 'ZTS']
     df_sp500 = get_all_filings(api_key, tickers)
     result = get_business_description(df_sp500, api_key, extractorApi)
+
+    print("RESULT: /n",result)
+
+     # Check if the descriptions are empty
+    if result['description'].apply(lambda x: len(x) == 0).all():
+        print("All descriptions are empty. Cannot train Word2Vec model.")
+        return
 
     # Preprocess the text
     result['description'] = result['description'].apply(preprocess_text)
@@ -158,11 +185,14 @@ def main():
     # Print the similarity matrix
     print(similarity_matrix)
 
+    # Create the minimum spanning tree
+    G = create_minimum_spanning_tree(similarity_matrix, result)
+
     # Wait for Elasticsearch and Neo4j to start
     time.sleep(30)  # Adjust this value based on how long it takes for Elasticsearch and Neo4j to start
 
     # Connect to Elasticsearch
-    es = Elasticsearch([{'host': 'elasticsearch', 'port': 9200}])
+    es = Elasticsearch([{'host': 'elasticsearch', 'port': 9200, 'scheme': 'http'}])
 
     # Connect to Neo4j
     neo4j_driver = GraphDatabase.driver("bolt://neo4j:7687", auth=basic_auth("neo4j", "password"))  # Replace with your Neo4j username and password
